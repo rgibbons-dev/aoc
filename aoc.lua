2ae8ae11e942a0e646775ee58277c6e59b432b83
ryan gibbons <amalgam.drawers.0k@icloud.com>
2024-01-12 17:00:03 -0500

    initial commit

---
+++ aoc.lua
@@ -0,0 +1,50 @@
+--[[ Navigating this file
+	outside of the "UTILS" section, there are groupings of code
+	separated by year. if you'd like to go to a specific year,
+	i suggest searching "YY BEGIN", where "YY" represents the
+	last two digits of the year of advent of code exercises.
+	if you'd like to check out a particular day, search
+	"YYDDP" where again, "YY" means last two digits of the year,
+	"DD" means the two digits represeting the day, and "P"
+	means represents the part of the problem. Happy reading!
+]]--
+-- UTILS BEGIN
+
+function split(file)
+	local lines = {}
+	for line in file.gmatch("[^\\n]") do
+		table.insert(lines, line)
+	end
+	return lines
+end
+
+function parse(path, tbl)
+	local file = io.open(path, "r")
+	if file then
+		local contents = file:read("a")
+		file:close()
+		if tbl then
+			return split(contents)
+		else
+			return contents
+		end
+	else
+		print("read failed")
+	end
+end
+
+-- UTILS END
+
+-- 23 BEGIN
+
+function _23011()
+	local contents = parse("./2023/one/input", false)
+	print(contents)
+end
+-- 23 END
+
+-- MAIN BEGIN
+
+
+
+-- MAIN END
---
+++ readme.md
@@ -0,0 +1,8 @@
+# advent of code solutions
+
+everything is in aoc.lua
+
+inputs -> yyyy/num/input
+notes -> yyyy/num/note.md
+
+(or read my thoughts as a [webpage](https://rgibbons-dev.github.io/aoc)
